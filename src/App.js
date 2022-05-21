2929fa53b88721edae95d167ac64b2c22fda7a67
korean_kat <katarina999@outlook.fr>
2022-05-21 02:57:04 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg"
+import "./App.css"
+
+function App() {
+  return (
+    <div className='App'>
+      Publish your podcasts everywhere. Upload your audio to Pod with a single
+      click. We’ll then distribute your podcast to Spotify, Apple Podcasts,
+      Google Podcasts, Pocket Casts and more! Request access
+    </div>
+  )
+}
+
+export default App
